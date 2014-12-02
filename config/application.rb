8c04f56c1a5d4e58b4df5d6463b1952b73f7aecd
carolvisoto <carolvisoto@gmail.com>
2014-12-02 10:13:53 +0100

    tring to run bootstrap

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Anchor
   class Application < Rails::Application
-    config.assets.enable = false
+    #config.assets.enable = false
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
