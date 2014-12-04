001263ff0666aebce6e26abcbfea21093b330fab
michelle <mverone@gmail.com>
2014-12-04 15:50:24 +0100

    hidde playlist in stories route

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -8,22 +8,23 @@ class StoriesController < ApplicationController
   def show
     client = SoundCloud.new(:client_id => '69e93cf2209402f6f3137a6452cf498f')
     @story  = client.get("/tracks/#{params[:current_track_id]}")
-    user_id = @story.user_id
-    @playlist = client.get("/users/#{user_id.to_s}/playlists/").first
+    # user_id = @story.user_id
+    # @user_playlists = client.get("/users/#{user_id.to_s}/playlists/")
   end
 
   def playlists
+
     client = SoundCloud.new(:client_id => '69e93cf2209402f6f3137a6452cf498f')
     @playlist = client.get("/playlists/#{params[:playlist_id]}") 
     @current_track_id = params[:current_track_id].to_i 
-
-    @story = @playlist.tracks.find do |track|
+    @story_with_playlist = @playlist.tracks.find do |track|
               track[:id] == params[:current_track_id].to_i
             end
-    render "show"
+    @story  = client.get("/tracks/#{params[:current_track_id]}")
   end
 
-  
+   
+
     # fetch the playlist from the soundcloud api
     # fetch the tracks element, which is an array
     # find the current track in the tracks in the playlist, set it to story
