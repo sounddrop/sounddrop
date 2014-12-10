d961c54a784ce68f0af6dcdfc65f64c31a89c40d
michelle <mverone@gmail.com>
2014-12-10 18:00:19 +0100

    created playlists and stories specs

---
+++ spec/features/homepage_page_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'rails_helper'
+require 'pry'
+
+
+# describe "Homepage" , type: :feature do
+#    it "displays " do
+#        visit ""
+#        # binding.pry
+#        expect(find_link("")).to be_visible
+#    end
+
+# end
---
+++ spec/features/playlists_page_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require 'rails_helper'
+require 'pry'
+
+
+describe "Playlist page" , type: :feature do
+   it "displays list of stories" do
+       visit "/playlists/63171096/180709118"
+       expect(find_link("Vinyl Sculpture")).to be_visible
+   end
+
+end
