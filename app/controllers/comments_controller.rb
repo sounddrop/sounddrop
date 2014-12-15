295fe12f932862ebc93f248ad4afed90c5f02f62
carolvisoto <carolvisoto@gmail.com>
2014-12-15 12:16:58 +0100

    make comments work in playlist and stories

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.save 
-    redirect_to root_path 
+    redirect_to :back  
   end
 
   def index
