0e1f70fd1f273907914dbf367089f0def09b8101
michelle <mverone@gmail.com>
2015-01-07 15:49:01 +0100

    Add users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141210155353) do
+ActiveRecord::Schema.define(version: 20150107144524) do
 
   create_table "comments", force: true do |t|
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.text "name"
   end
 
 end
