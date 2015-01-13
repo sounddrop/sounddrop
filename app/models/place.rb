e8c797677319c59777ed1dee5f5158ca53dde333
carolvisoto <carolvisoto@gmail.com>
2015-01-13 16:16:01 +0100

    create table  place

---
+++ app/models/place.rb
@@ -0,0 +1,3 @@
+class Place < ActiveRecord::Base
+  has_many :user
+end
---
+++ db/migrate/20150112124913_create_places.rb
@@ -0,0 +1,10 @@
+class CreatePlaces < ActiveRecord::Migration
+  def change
+    create_table :places do |t|
+      t.string :name
+      t.integer :sc_playlist
+
+      t.timestamps
+    end
+  end
+end
