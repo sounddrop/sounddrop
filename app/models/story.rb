dee3c74e06d147c5c11bfda67eacab1e63860a25
BriocheBerlin <bri.ma@gmx.de>
2015-01-20 16:03:33 +0100

    Makes test for like button work, should really make build pass.

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,4 +2,8 @@ class Story < ActiveRecord::Base
   has_many :votes, dependent: :destroy
   belongs_to :user
   belongs_to :place
+
+  def dennis
+    1
+  end
 end
--- spec/features/stories_page_spec.rb
+++ spec/features/stories_page_spec.rb
@@ -30,19 +30,18 @@ describe "Story page" , type: :feature do
     expect(page).to have_selector("input")
   end
 
-  feature "comment", js: true do
-    scenario "write a comment and send it" do
-      visit "/stories/178594490"
-      fill_in "new_comment", with: "Test comment"
-      click_button "Send Comment"
-      expect(page).to have_content("Test comment")
-    end
+  scenario "write a comment and send it", js: true do
+    visit "/stories/178594490"
+    fill_in "new_comment", with: "Test comment"
+    click_button "Send Comment"
+    expect(page).to have_content("Test comment")
   end
 
-  feature "like button", js: true, :skip => "Database locked, there seems to be some concurrency problem due to ajax, not fixed yet." do
-    scenario "increases the likes of a story" do
-      visit "/stories/178594490"
-      expect {click_button "like"}.to change{Vote.count}.by(1)
-    end
+  scenario "klicking the like button increases the likes of a story", js: true do
+    visit "/stories/178594490"
+    likes_before = page.find("#votes").text.to_i
+    click_button "like"
+    likes_now = page.find("#votes").text.to_i
+    expect(likes_now - likes_before).to eq(1)
   end
 end
