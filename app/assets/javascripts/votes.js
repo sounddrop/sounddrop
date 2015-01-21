4d73043dd33c60affd8f65612363e6e59bab7605
Michelle Veronese <mverone@gmail.com>
2015-01-21 20:03:40 +0100

    Remove duplicate code and make ajax call work the rails way

--- app/assets/javascripts/votes.js
+++
@@ -1,8 +0,0 @@
-$("#like_button").click(function(event) {
-  event.preventDefault();
-  $.ajax ({
-    url: '/stories/<%=@story.id %>/upvote', method: 'POST'
-  }).success(function(result) {
-    $("#votes").html(result.count_votes);
-  });
-});
