07931d3d75030dafb251f22f810dbcc752c54f29
BriocheBerlin <bri.ma@gmx.de>
2015-01-27 13:01:51 +0100

    Changes script for Travis

--- spec/features/homepage_page_spec.rb
+++ spec/features/homepage_page_spec.rb
@@ -1,12 +1,12 @@
-# require 'spec_helper'
-# require 'rails_helper'
-# require 'pry'
+require 'spec_helper'
+require 'rails_helper'
+require 'pry'
 
 
-# describe "Home page" , type: :feature do
-#    it "says welcome " do
-#       visit "/"
-#       expect(page).to have_content "Welcome to SoundDrop"
-#    end
+describe "Home page" , type: :feature do
+  it "says welcome " do
+    visit "/"
+    expect(page).to have_content "SoundDrop lets you record stories and connect them to a place where they belong - via QR code or geolocation."
+  end
+end
 
-# end
