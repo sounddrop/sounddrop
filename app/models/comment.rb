ab9c499776f1810ba7373c7b2bd263f1c89162ad
BriocheBerlin <bri.ma@gmx.de>
2015-02-04 12:53:51 +0100

    Adds validations for comments and places, tests them.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,3 @@
 class Comment < ActiveRecord::Base
+  validates :text, presence: true
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ActiveRecord::Base
   has_many :users
   has_many :stories
-  
+  validates_associated :stories
   validates :name, presence: true, uniqueness: true
 end
