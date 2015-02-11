3257eb81c939144a1e37f87e117de0520d2d6c19
carolvisoto <carolvisoto@gmail.com>
2015-02-11 11:56:37 +0100

    refactored place name code from views to controllers

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -10,21 +10,13 @@ class StoriesController < ApplicationController
 
   def show
     client = SoundCloud.new(:client_id => '69e93cf2209402f6f3137a6452cf498f') 
- 
     @story = Story.find_by_sc_track(params[:sc_track])
+    display_place(@story)
     if @story != nil
       @story_at_sc = client.get("/tracks/#{params[:sc_track]}")
       diplay_image(@story_at_sc)
     end
   end
-
-  def diplay_image(story_at_sc)
-      @artwork = story_at_sc.artwork_url
-      if @artwork == nil
-        @artwork = story_at_sc.user.avatar_url
-      end 
-      @artwork.sub! "large", "crop"
-  end  
 
   def upvote
     @story = Story.find(params[:id])
@@ -42,6 +34,7 @@ class StoriesController < ApplicationController
 
   def playlists
     @story = Story.find_by_sc_track(params[:sc_track])
+    display_place(@story)
     client = SoundCloud.new(:client_id => '69e93cf2209402f6f3137a6452cf498f')
     @playlist = client.get("/playlists/#{params[:playlist_id]}") 
     @current_track_id = params[:sc_track].to_i 
@@ -49,9 +42,35 @@ class StoriesController < ApplicationController
       track[:id] == params[:sc_track].to_i
     end
     @playlist.tracks.each do |track|
-      if track.id == @story_at_sc.id 
-        diplay_image(@story_at_sc) 
+      begin
+        if track.id == @story_at_sc.id 
+          diplay_image(@story_at_sc) 
+        end
+      rescue Exception => e
+        e.message
       end
     end
   end
+
+  def diplay_image(story_at_sc)
+      @artwork = story_at_sc.artwork_url
+      if @artwork == nil
+        @artwork = story_at_sc.user.avatar_url
+      end 
+      @artwork.sub! "large", "crop"
+  end  
+
+  def display_place(story)
+    begin
+      unless story.sc_track.nil?
+        place = Place.find_by(story.sc_track)
+        if place != nil
+         @place_name = place.name
+        end
+      end
+    rescue Exception => e
+      e.message
+    end 
+  end 
+
 end
