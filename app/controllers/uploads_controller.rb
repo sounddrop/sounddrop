9806849dc0d8b2c2be4dbd9c1d16fa47db8eecb6
Michelle Veronese <mverone@gmail.com>
2015-02-11 17:05:48 +0100

    Create new action and js fucntion to get id_track before upload it

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,4 +1,4 @@
-class AuthenticationsController < ApplicationController
+class UploadsController < ApplicationController
   
   require 'soundcloud'
 
@@ -18,14 +18,30 @@ class AuthenticationsController < ApplicationController
     access_token_hash = client.exchange_token(:code => code)
     session[:access_token_hash] = access_token_hash
     # puts "this is the session: " + session[:access_token_hash].to_s
-    redirect_to my_stories_path
+    redirect_to list_tracks_path
   end
 
-  def my_stories
+  def list_tracks
     # client = Soundcloud.new(:access_token => 'YOUR_ACCESS_TOKEN')
     client = Soundcloud.new(:access_token => session[:access_token_hash]["access_token"])
     @current_user = client.get('/me')
     @current_user_tracks = client.get('/me/tracks')
+  end
+  
+  def upload_track
+    user_id = params[:user_id]
+    track_id = params[:sc_track]
+    track_title = params[:title]
+    # place_id = params[:place]
+
+    @story = Story.create(sc_track: params[:sc_track], title: params[:title])
+
+    redirect_to upload_done_path
+  end
+
+  def upload
+    render json: { }
+  
   end
 
   
