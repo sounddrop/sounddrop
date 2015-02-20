83d0822a6191493094d70e790cc238c5811131e8
BriocheBerlin <bri.ma@gmx.de>
2015-02-20 12:11:36 +0100

    Fixes broken tests.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@
     sc_place = Place.create!(name: "@SoundCloud HQ")
     sc_place = Place.create!(name: "@SoundCloud SF")
     sc_place = Place.create!(name: "@SoundCloud NY")
-    story1 = Story.create!(sc_track:"187471639", place_id: sc_place.id, title:"Coffee Machine")
-    story2 = Story.create!(sc_track:"187472172", place_id: sc_place.id, title: "Brewing Coffee for engineers")
-    story3 = Story.create!(sc_track:"187472038", place_id: sc_place.id, title: "Art installation")
 
-  
+    story1 = Story.create!(sc_track:"187471639", place_id:"1", title:"Coffee Machine")
+    story2 = Story.create!(sc_track:"187472172", place_id:"1", title: "Brewing Coffee for engineers")
+    story3 = Story.create!(sc_track:"187472038", place_id:"1", title: "Art installation")
+
