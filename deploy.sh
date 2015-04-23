b1fc015af9b1426f5a2ebe2be6449d7181004a13
Duana Saskia <starkcoffee+apps@gmail.com>
2015-04-23 18:31:22 +0200

    Fix deploy script to terminate on first error

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+
 ssh-add
 ssh -A root@sounddrop.audio <<END
+
+set -e -x
 cd /home/rails
 git pull
 bundle install --deployment
 chown -R rails:www-data .
 service unicorn restart
+
 END
