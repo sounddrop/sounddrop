ee0df791774ad30e750ec99a5280aa37806e4075
berlintam <tam.eastley@makerist.de>
2015-08-03 19:45:49 +0200

    make sure redirects work on production

--- app/controllers/connect_controller.rb
+++ app/controllers/connect_controller.rb
@@ -6,19 +6,25 @@ class ConnectController < ApplicationController
   end
 
   def login
-    client = SoundCloud.new({:client_id => '69e93cf2209402f6f3137a6452cf498f',
-                    :client_secret => '8ca711ab13836dc62482164d3a952eda',
-                    :redirect_uri => 'http://localhost:3000/verify'})
+    client = SoundCloud.new({ client_id: '69e93cf2209402f6f3137a6452cf498f',
+                              client_secret: '8ca711ab13836dc62482164d3a952eda',
+                              redirect_uri: environment_url })
     redirect_to client.authorize_url()
   end
 
   def verify
-    client = SoundCloud.new({:client_id => '69e93cf2209402f6f3137a6452cf498f',
-                    :client_secret => '8ca711ab13836dc62482164d3a952eda',
-                    :redirect_uri => 'http://localhost:3000/verify'})
+    client = SoundCloud.new({ client_id: '69e93cf2209402f6f3137a6452cf498f',
+                              client_secret: '8ca711ab13836dc62482164d3a952eda',
+                              redirect_uri: environment_url })
     code = params[:code]
     access_token_hash = client.exchange_token(:code => code)
     session[:access_token_hash] = access_token_hash
     redirect_to new_drop_path
+  end
+
+  private
+
+  def environment_url
+    Rails.env.production? ? "http://sounddrop.audio/verify" : "http://localhost:3000/verify"
   end
 end
