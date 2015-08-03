14f2a4bbd7572732a8a19fda211e03851a155318
Franziska Wittleder <franziska.wittleder@gmail.com>
2015-08-03 18:48:53 +0200

    url feature when creating new drop

--- app/controllers/drops_controller.rb
+++ app/controllers/drops_controller.rb
@@ -45,13 +45,13 @@ class DropsController < ApplicationController
 
   def upvote
     @drop = Drop.find(params[:id])
-    if session[:liked_stories].nil?
-      session[:liked_stories] = []
+    if session[:liked_drops].nil?
+      session[:liked_drops] = []
     end
     unless
-      session[:liked_stories].include?(@drop.id)
+      session[:liked_drops].include?(@drop.id)
       @create_votes = @drop.votes.create
-      session[:liked_stories] << @drop.id
+      session[:liked_drops] << @drop.id
     end
     @count_votes = @drop.votes.count
     render json: {count_votes: @count_votes, user_session: session[:liked_stories].inspect}
