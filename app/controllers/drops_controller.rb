b7b30fc0e5aef09eaeb181d23e50231c91d76e2f
Franziska Wittleder <franziska.wittleder@gmail.com>
2015-08-10 20:30:05 +0200

    add login redirect when not logged in plus massage

--- app/controllers/drops_controller.rb
+++ app/controllers/drops_controller.rb
@@ -7,21 +7,15 @@ class DropsController < ApplicationController
   end
 
   def new
-    client = Soundcloud.new(:access_token => session[:access_token_hash]["access_token"])
-    begin
+    if session[:access_token_hash].nil?
+      render :login_redirect
+    else
+      client = Soundcloud.new(:access_token => session[:access_token_hash]["access_token"])
       @current_user = client.get('/me')
-    rescue => e
-     if e.response.code == 401
-        redirect_to login_url
-      else 
-        render plain: "There was some error, namely #{e.message}"
-     end
-  end
-
-
-    #@current_user_tracks = client.get('/me/tracks')
-    #@drop = Drop.new
-    #@places = Place.all
+      @current_user_tracks = client.get('/me/tracks')
+      @drop = Drop.new
+      @places = Place.all
+    end
   end
 
   def create
