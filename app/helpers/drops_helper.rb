6ed0ccedba70f3b2049011b869a56b5cd6d8ea97
Nynne <nynnejc@gmail.com>
2015-09-18 14:16:10 +0200

    google map added to drops

--- app/helpers/drops_helper.rb
+++ app/helpers/drops_helper.rb
@@ -4,4 +4,9 @@ module DropsHelper
       "Coordinates : #{place.latitude} / #{place.longitude}"
     end
   end
+  def google_map_parameter(place)
+    if place.longitude && place.latitude
+      "#{place.longitude},#{place.latitude}"
+    end
+  end
 end
