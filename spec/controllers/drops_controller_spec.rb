adfcd994c065f234b1da028689d396321e2e1e29
Nynne <nynnejc@gmail.com>
2015-09-30 17:13:03 +0200

    refacturing tests

--- spec/controllers/drops_controller_spec.rb
+++ spec/controllers/drops_controller_spec.rb
@@ -6,20 +6,26 @@ require 'pry'
 describe DropsController do
 
   it "returns 200 if the drop exists" do
-     get :show , {'id' => '187471639'}
+     drop = create(:drop)
+     get :show , {'id' => drop.sc_track}
      expect(@response.status).to eq(200)
   end
-  it "creates the drop if the places already exists" do
+  it "creates a drop at a place" do
     get :create, {
       'sc_url' => 'https://soundcloud.com/prince3eg/prince-3rdeyegirl-the-look-live-in-toronto-05-19-15',
       'drop' => {
         'place' => {
-          'name' => Place.first.name
+          'name' => "himmel",
+          'latitude' => 12.0,
+          'longitude' => 12.0
         }
       }
     }
     expect(@response.status).to eq(302)
-    expect(Drop.last.place).to eq(Place.first)
+    place = Drop.last.place
+    expect(place.name).to eq("himmel")
+    expect(place.latitude).to eq(12.0)
+    expect(place.longitude).to eq(12.0)
   end
   it "returns 404 if the drop doesn't exist" do
      get :show , {'id' => '666'}
