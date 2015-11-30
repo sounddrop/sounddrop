a121b5ba45f6b39c8774d211f454e453c81bcc2b
berlintam <tam.eastley@makerist.de>
2015-11-30 20:21:48 +0100

    add geocoded gem & behaviour

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ActiveRecord::Base
   has_many :drops
   validates_associated :drops
+
   validates :name, presence: true, uniqueness: true
-  validates :latitude, presence: true
-  validates :longitude, presence: true
+  validates :location, presence: true
+
+  geocoded_by :location
+  after_validation :geocode
 end
