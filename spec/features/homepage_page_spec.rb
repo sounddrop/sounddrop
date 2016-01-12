138b8905c5e6152ef0cd241080922c2ca7f8be79
nynnejc <nynnejc@gmail.com>
2016-01-12 16:52:03 +0100

    change in test

--- spec/features/homepage_page_spec.rb
+++ spec/features/homepage_page_spec.rb
@@ -6,7 +6,7 @@ require 'pry'
 describe 'Home page' , type: :feature do
   it 'says welcome' do
     visit "/"
-    expect(page).to have_content "SoundDrop lets you record stories and connect them to a place where they belong - via QR code or geolocation."
+    expect(page).to have_content "Welcome to SoundDrop!"
   end
 
   it 'has a navigation bar' do
