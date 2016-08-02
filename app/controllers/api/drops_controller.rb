07e2ab20730e890c530e69ea156545746ed601dc
Dayana Mick <dayanamick@gmail.com>
2016-08-02 17:58:00 +0200

    Add active record queries to retrieve drops within radius

--- app/controllers/api/drops_controller.rb
+++ app/controllers/api/drops_controller.rb
@@ -3,6 +3,9 @@ class Api::DropsController < ApplicationController
 
   def index
     if params[:longitude] && params[:latitude]
+      places_within_radius = Place.near([52.5380655, 13.3939808], 1, :units => :km)
+      @drops = Drop.where(place_id: places_within_radius)
+      #ids = Ids.all
       #give back drops within radius 1 km: Place.near([40.71, -100.23], 20, :units => :km)
     else
       @drops = Drop.all
