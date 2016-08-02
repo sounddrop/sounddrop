ec5c6dde1cf5ebe97afe63a1a4dcbf64fc7d2922
Dayana Mick <dayanamick@gmail.com>
2016-08-02 14:38:24 +0200

    Create traits for FactoryGirl to create drops with different locations
    Begin with the spec test

--- spec/factories/place.rb
+++ spec/factories/place.rb
@@ -2,6 +2,12 @@ FactoryGirl.define do
   factory :place do
     location 'SoundCloud, Berlin'
     sequence(:name) { |n| "Place #{n}" }
+    trait :place_australia
+      location: "Sydney"
+      trait :place_near_soundcloud
+        location: "Mauerpark, Berlin"
+        trait :place_
+          location: ""
+    end
   end
 end
-
