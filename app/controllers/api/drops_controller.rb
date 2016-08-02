6401e3cb79bc10bd705f69599ad6c94b8c8d1a30
Powerglove <lang.joh.jl@gmail.com>
2016-08-02 16:45:36 +0200

    start adding radius to index action, WIP

--- app/controllers/api/drops_controller.rb
+++ app/controllers/api/drops_controller.rb
@@ -2,6 +2,10 @@ class Api::DropsController < ApplicationController
   respond_to :json
 
   def index
-    @drops = Drop.all
+    if params[:longitude] && params[:latitude]
+      #give back drops within radius 1 km: Place.near([40.71, -100.23], 20, :units => :km)
+    else
+      @drops = Drop.all
+    end
   end
 end
