325299aba9b3f0a9316698cc2dc860ca2466706f
Powerglove <lang.joh.jl@gmail.com>
2016-08-02 16:43:16 +0200

    correct trait syntax

--- spec/factories/place.rb
+++ spec/factories/place.rb
@@ -2,12 +2,11 @@ FactoryGirl.define do
   factory :place do
     location 'SoundCloud, Berlin'
     sequence(:name) { |n| "Place #{n}" }
-    trait :place_australia
-      location: "Sydney"
-      trait :place_near_soundcloud
-        location: "Mauerpark, Berlin"
-        trait :place_
-          location: ""
+    trait :place_australia do
+      location "Sydney"
     end
+      trait :place_near_soundcloud do
+        location "U Bernauer Str., Berlin"
+      end
   end
 end
