58358b24432a577f56c7209c1bda2e21b1b29d64
Powerglove <lang.joh.jl@gmail.com>
2016-08-04 16:49:54 +0200

    add more exact place trait and add lat and long for trait Bernauer

--- spec/factories/place.rb
+++ spec/factories/place.rb
@@ -3,13 +3,15 @@ FactoryGirl.define do
     location 'SoundCloud, Berlin'
     sequence(:name) { |n| "Place #{n}" }
     trait :place_australia do
-      location "Sydney"
+      location "209 Clarence St, Sydney"
+      latitude -33.869717
+      longitude 151.205068
     end
-    trait :place_near_soundcloud do
-      location "U Bernauer Str., Berlin"
-      latitude 52.538255
-      longitude 13.396255
-    end
+      trait :place_near_soundcloud do
+        location "U Bernauer Str., Berlin"
+        latitude 52.538255
+        longitude 13.396255
+      end
   end
 end
 
