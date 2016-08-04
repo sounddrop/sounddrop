faad0aa4e8b0cf337cc3ec99eccc36b2fc4b557f
Powerglove <lang.joh.jl@gmail.com>
2016-08-04 16:50:22 +0200

    debugging because of nil error

--- spec/requests/api/drops_controller_spec.rb
+++ spec/requests/api/drops_controller_spec.rb
@@ -19,6 +19,7 @@ RSpec.describe Api::DropsController, :vcr, type: :request do
       it "delivers valid JSON" do
         get "/api/drops"
         parsed_response = ActiveSupport::JSON.decode(response.body)
+        puts parsed_response
         expect(parsed_response).to be_a Enumerable
       end
 
@@ -34,12 +35,40 @@ RSpec.describe Api::DropsController, :vcr, type: :request do
     describe "GET /api/drops" do
 
       context 'with latitude and longitude given' do
-        let!(:place) { FactoryGirl.create :place, :place_near_soundcloud }
-        let!(:drop) { FactoryGirl.create :drop, :drop_at_Bernauer }
+        let!(:place_near_soundcloud) { FactoryGirl.create :place, :place_near_soundcloud}
+        let!(:place_australia) { FactoryGirl.create :place, :place_australia }
+        let!(:drop_at_Bernauer) { FactoryGirl.create :drop, :drop_at_Bernauer}
+        let!(:drop_in_sydney) { FactoryGirl.create :drop, :drop_in_sydney}
 
-        it "returns the drops that are within a radius of 1 Km" do
+
+        it "does not return drops outside of radius" do
+          expect(place_near_soundcloud).to be_geocoded
+          puts "Near soundcloud:"
+          puts place_near_soundcloud.to_coordinates
+
+          puts "Australia"
+          expect(place_australia).to be_geocoded
+          puts place_australia.to_coordinates
+
+          puts "Drop bernauer"
+          expect(drop_at_Bernauer.place).to be_geocoded
+          puts drop_at_Bernauer.place.to_coordinates
+#
+          puts "Drop syndey"
+          expect(drop_in_sydney.place).to be_geocoded
+          puts drop_in_sydney.place.to_coordinates
+
+          puts "Distance"
+          class ANiceClass
+            include Geocoder::Calculations
+          end
+          distance = ANiceClass.new.distance_between(drop_at_Bernauer.place.to_coordinates, place_near_soundcloud.to_coordinates)
+          puts distance.to_s
+
+
           get "/api/drops?latitude=52.537016&longitude=13.394861"
           parsed_response = ActiveSupport::JSON.decode(response.body)
+          #puts parsed_response
           expect(parsed_response[0].keys).to include("id")
         end
 
