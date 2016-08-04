0b4fdbc10a4d2f49ba37367bc4b5a821e4720355
Powerglove <lang.joh.jl@gmail.com>
2016-08-04 16:46:41 +0200

    debugging and delete comments

--- app/controllers/api/drops_controller.rb
+++ app/controllers/api/drops_controller.rb
@@ -5,12 +5,17 @@ class Api::DropsController < ApplicationController
     #if params[:longitude]  && params[:latitude]
     #  @drops = Drop.all
       #raise params[:longitude].inspect
-      places_within_radius = Place.near(params[:longitude], params[:latitude],  10, units:  :km)
-      @drops = Drop.where(place_id: places_within_radius)
-      #ids = Ids.all
-      #give back drops within radius 1 km: Place.near([40.71, -100.23], 20, :units => :km)
+      puts "Im Controller: #{params.inspect}"
+      
+      
+
+      places_within_radius = Place.near([params[:longitude].to_f, params[:latitude].to_f],  10, units:  :km)
+      
+      puts places_within_radius.inspect
+      puts "----"
+      @drops = Drop.where(place_id: places_within_radius.map(&:id))
+
     #end
   end
 end
-# Long and lat als Float and dann darein packen
-# spielen mit dem Radius
+
