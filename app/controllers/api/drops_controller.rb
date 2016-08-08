9da2ae44c828e9001b4a3284bd2c4605e04bce89
Powerglove <lang.joh.jl@gmail.com>
2016-08-08 15:28:23 +0200

    add if statement to index action

--- app/controllers/api/drops_controller.rb
+++ app/controllers/api/drops_controller.rb
@@ -2,19 +2,12 @@ class Api::DropsController < ApplicationController
   respond_to :json
 
   def index
-    #if params[:longitude]  && params[:latitude]
-    #  @drops = Drop.all
-      #raise params[:longitude].inspect
-      #puts "Im Controller: #{params.inspect}"
-   
-
+    if params[:latitude] && params[:longitude]
       places_within_radius = Place.near([params[:latitude], params[:longitude]],  10, units:  :km)
-      
-      #puts places_within_radius.inspect
-      #puts "----"
       @drops = Drop.where(place_id: places_within_radius.map(&:id))
-
-    #end
+    else
+      @drops = Drop.all
+    end
   end
 end
 
