d4b91bfcccf634e881d51afccd7b5b85a627fe83
Dayana Mick <dayanamick@gmail.com>
2016-08-10 10:43:11 +0200

    Add logical expression in case no radius param gets passed

--- app/controllers/api/drops_controller.rb
+++ app/controllers/api/drops_controller.rb
@@ -3,11 +3,11 @@ class Api::DropsController < ApplicationController
 
   def index
     if params[:latitude] && params[:longitude]
-      places_within_radius = Place.near([params[:latitude], params[:longitude]],  10, units:  :km)
+      radius = params[:radius].presence || 10
+      places_within_radius = Place.near([params[:latitude], params[:longitude]], radius, units:  :km)
       @drops = Drop.where(place_id: places_within_radius.map(&:id))
     else
       @drops = Drop.all
     end
   end
 end
-
