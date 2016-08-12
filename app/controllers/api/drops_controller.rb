313f4797f6d3cf17d1ef137603c93573df808609
Powerglove <lang.joh.jl@gmail.com>
2016-08-12 14:41:22 +0200

    add default radius constant

--- app/controllers/api/drops_controller.rb
+++ app/controllers/api/drops_controller.rb
@@ -1,11 +1,12 @@
 # We use .map instead of .pluck, as .pluck modifies the SELECT clause and produces strange results.
 
 class Api::DropsController < ApplicationController
+  DEFAULT_RADIUS = 10
   respond_to :json
 
   def index
     if params[:latitude] && params[:longitude]
-      radius = params[:radius].presence || 10
+      radius = params[:radius].presence || DEFAULT_RADIUS
       places_within_radius = Place.near([params[:latitude], params[:longitude]], radius, units:  :km)
       @drops = Drop.where(place_id: places_within_radius.map(&:id))
     else
