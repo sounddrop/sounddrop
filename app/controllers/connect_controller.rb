418446f582c6b6da9ba6a5e7f351033e169f1c59
berlintam <tam.eastley@makerist.de>
2016-08-19 12:57:34 +0200

    hopefully fix expired token error

--- app/controllers/connect_controller.rb
+++ app/controllers/connect_controller.rb
@@ -9,7 +9,7 @@ class ConnectController < ApplicationController
     client = SoundCloud.new({ client_id: ENV['SOUNDCLOUD_CLIENT_ID'],
                               client_secret: ENV['SOUNDCLOUD_CLIENT_SECRET'],
                               redirect_uri: environment_url })
-    redirect_to client.authorize_url()
+    redirect_to client.authorize_url(scope: 'non-expiring')
   end
 
   def verify
