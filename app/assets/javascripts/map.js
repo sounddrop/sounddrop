e94bf920e9c4dff3222865e5e4d303e08578c6c8
Powerglove <lang.joh.jl@gmail.com>
2016-08-24 15:09:29 +0200

    move variables and if clause outside of google object , convert lat and long to floats, comment out code that still needs to be implemented

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,28 +2,40 @@ var Map = {
   map: null,
   setMarker: function() {},
   init: function() {
+    var dropLat, dropLong;
+    var dropContainerNode = document.querySelector("[data-drop-track]");
+    if (dropContainerNode !== null) {
+      dropLat = parseFloat(dropContainerNode.dataset.dropLat);
+      dropLong = parseFloat(dropContainerNode.dataset.dropLong);
+    } else {
+       dropLat = 52.536;
+       dropLong = 13.392;
+    }
     Map.map = new google.maps.Map(document.getElementById('map'), {
-      var dropContainerNode = document.querySelector("[data-drop-track]");
-      if (dropContainerNode !== null) {
-        zoom: 15,
-        center: { lat: dropContainerNode.dataset.dropLat, lng: dropContainerNode.dataset.dropLong }
-      }
-    };
-  }
-  changeMap: function() {},
+      zoom: 15,
+      center: { lat: dropLat, lng: dropLong }
+    });
+  },
+
   createMarker: function(){
     var icon = {
       url: soundDropMarker,
       scaledSize: new google.maps.Size(50, 50)
     };
 
-
-
   }
+
 };
 
-var markers = [ ];
 
 
-setMarkers: function() {}
-createMarkers: function() {}
+//var markers = [ ];
+// setMarkers: function() {}
+// createMarkers: function() {}
+
+ // var marker = new google.maps.Marker({
+  //   // maps API position, what is map.getCenter return
+  //   position: map.getCenter(),
+  //   icon: icon,
+  //   map: map
+  // });
