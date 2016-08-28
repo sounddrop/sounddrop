dc2c2ecbd28a80533eb761b9ebad33e88fb649bd
Carsten Zimmermann <cz@aegisnet.de>
2016-08-28 21:24:45 +0200

    Run `rake rails:update`

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,12 +2,17 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path
+Rails.application.config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
+
 Rails.application.config.assets.precompile += %w( highcharts.js )
 Rails.application.config.assets.precompile += %w( stats.js )
 Rails.application.config.assets.precompile += %w( comments.js )
 Rails.application.config.assets.precompile += %w( upvote.js )
 Rails.application.config.assets.precompile += %w( play.js )
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
