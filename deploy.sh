a8f3855df7ad37adf869cf35ede794a1da04c63d
Dayana Mick <dayanamick@gmail.com>
2016-09-02 11:50:14 +0200

    Modify deployment script to stop unicorn before bundle install

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ ssh -A root@sounddrop.audio <<END
 set -e -x
 cd /home/rails
 git pull
+service unicorn stop
 bundle install --deployment
 chown -R rails:www-data .
-service unicorn stop
 bundle exec rake assets:precompile
 bundle exec rake db:migrate
 service unicorn start
