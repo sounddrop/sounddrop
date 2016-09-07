942e3c625a7a21e2c0a1becacbeea316027a4e9e
Dayana Mick <dayanamick@gmail.com>
2016-09-07 16:56:10 +0200

    Add virtual attributes for all_tags in the model, permit all_tags in the controller, add select for all_tags

--- app/models/drop.rb
+++ app/models/drop.rb
@@ -18,7 +18,6 @@ class Drop < ActiveRecord::Base
       end
     end
 
-
   def image_from_track
     if soundcloud_track.artwork_url.nil?
       soundcloud_track.user.avatar_url.sub! 'large', 'crop'
@@ -38,5 +37,15 @@ class Drop < ActiveRecord::Base
 
   def place_location
     place.try(:location)
+  end
+
+  def all_tags=(names)
+    self.tags = names.split(",").map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
+
+  def all_tags
+    self.tags.map(&:name).join(", ")
   end
 end
