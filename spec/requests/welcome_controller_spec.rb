05422b522acbbced6675be8508ddb93efb9308f4
Powerglove <lang.joh.jl@gmail.com>
2016-09-13 15:48:56 +0200

    create one tag and two drop factories; correct matcher still to find

--- spec/requests/welcome_controller_spec.rb
+++ spec/requests/welcome_controller_spec.rb
@@ -2,16 +2,21 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe WelcomeController do 
-  context 'with several drops present' do
+  
+  context 'with several drops present and tag "sound" selected' do
 
     describe "GET /" do
 
-      let!(:drops_with_place)  { create_list(:drop, 2, :with_place) } #with tag
-
+      let!(:tag) {create :tag, name: "sound"}
+      let!(:drop_without_tag) {create :drop}
+      let!(:drop_with_tag) {create :drop, all_tags: [tag.name]}
 
       it "displays only drops with selected tag" do
-        get welcome_path
-        expect(response.status).to eq(200)
+        p drop_with_tag.tags
+        get "/?tag=sound"
+        p @response.body
+        @response = @response.to_a
+        expect(@response.length).to eq(1)
       end
     end
 
