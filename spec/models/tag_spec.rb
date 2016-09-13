0169c2304447439535a22c0d831f09b344e2d4a8
Powerglove <lang.joh.jl@gmail.com>
2016-09-13 16:48:33 +0200

    add association test

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Tag do
+ 
+  describe 'associations' do
+    it { is_expected.to have_many(:drops).through(:taggings) }
+  end
 end
