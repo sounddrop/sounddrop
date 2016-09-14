cbca1b6e03e3bc958ead060ba873e9e85922e2ba
Powerglove <lang.joh.jl@gmail.com>
2016-09-14 16:34:22 +0200

    constrain index action to only retrieve drops with selected tag from DB

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,22 @@
 class WelcomeController < ApplicationController
 
   def index
-    @drops = Drop.all
+    @drops =  if params[:tag]
+                selected_tag.drops
+              else
+                Drop.all
+              end
   end
 
   def about
 
+  end
+
+
+    private
+
+
+  def selected_tag
+    Tag.find_by(name: params[:tag].to_s) 
   end
 end
