57c64c7e1208a90856b41fda70e85dffe3cceb3f
Carsten Zimmermann <cz@aegisnet.de>
2016-09-15 10:25:30 +0200

    Fix css selector
    Apparently Rails 4.2.7.x includes the asset timestamp

--- spec/features/homepage_page_spec.rb
+++ spec/features/homepage_page_spec.rb
@@ -1,7 +1,6 @@
-require 'spec_helper'
 require 'rails_helper'
 
-describe 'Home page', type: :feature, :vcr => {:cassette_name => "place" }  do
+describe 'Home page', type: :feature do
   it 'says welcome' do
     visit "/"
     expect(page).to have_content "Welcome to SoundDrop!"
@@ -9,7 +8,7 @@ describe 'Home page', type: :feature, :vcr => {:cassette_name => "place" }  do
 
   it 'has a navigation bar' do
     visit "/"
-    expect(page).to have_css("img[src*='/assets/sounddrop-logo-name.png']")
+    expect(page).to have_css("img[src^='/assets/sounddrop-logo-name-']")
   end
 end
 
