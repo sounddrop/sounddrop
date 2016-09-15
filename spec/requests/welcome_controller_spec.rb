691864b09e37864212e495ff3a8b7b3404e244e7
Powerglove <lang.joh.jl@gmail.com>
2016-09-15 11:10:00 +0200

    add more test data to make test more precise

--- spec/requests/welcome_controller_spec.rb
+++ spec/requests/welcome_controller_spec.rb
@@ -3,15 +3,17 @@ require 'rails_helper'
 
 describe WelcomeController do 
   
-  context 'with two drops present and tag "sound" selected' do
+  context 'with several drops present and tag "sound" selected' do
 
     describe "GET /" do
 
-      let!(:tag) {create :tag, name: "sound"}
+      let!(:tag_sound) {create :tag, name: "sound"}
+      let!(:tag_story) {create :tag, name: "story"}
       let!(:drop_without_tag) {create :drop}
-      let!(:drop_with_tag) {create :drop, all_tags: [tag.name]}
+      let!(:drop_with_tag_sound) {create :drop, all_tags: ["sound"]}
+      let!(:drop_with_tag_story) {create :drop, all_tags: ["story"]}
 
-      it "displays only drops with selected tag" do
+      it "displays only drops with the tag sound" do
         get "/?tag=sound"
         expect(assigns(:drops).count).to eq(1)
       end
