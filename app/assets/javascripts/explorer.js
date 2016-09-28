a9fe711c45fa8604ca2dc5999a5b4b94d7412de3
Dayana Mick <dayanamick@gmail.com>
2016-09-28 22:50:56 +0200

    Add function to Javascript so upon clicking link, JSON response is delivered
    Add click handler
    Make div for response 50% wide
    Add div for links on the left
    Style links

--- app/assets/javascripts/explorer.js
+++ app/assets/javascripts/explorer.js
@@ -1,11 +1,51 @@
 $(function() {
-  $("#request").keyup(function(e) {
-    var path = e.target.value;
-    console.log(path);
+
+  var makeRequest = function(e) {
+    var path = $("#request").val();
+    console.log(path)
     $.getJSON(path, function(response) {
       var responseContainer = $("#explorer-response");
       var prettyResponse = JSON.stringify(response, null, "    ");
       responseContainer.html(prettyResponse);
     });
-  });
+  }
+
+  $("#request").keyup(makeRequest);
+
+  $('.endpoints').click(function(e) {
+    e.preventDefault();
+    makeRequest($(this)attr("href"))
+  })
+
+
+
 });
+
+
+
+
+// $(function() {
+//   $("#request").keyup(function(e) {
+//     var path = e.target.value;
+//     console.log(path);
+//   });
+// });
+//
+// $('.endpoints').click(
+//   function(e){
+//     e.preventDefault();
+//     var path = $(this).attr("href");
+//     $.getJSON(path, function(response) {
+//       var responseContainer = $("#explorer-response");
+//       var prettyResponse = JSON.stringify(response, null, "    ");
+//       responseContainer.html(prettyResponse);
+//       });
+//   }
+//
+//   function makeRequest(path) {
+//     $.getJSON(path, function(response) {
+//       var responseContainer = $("#explorer-response");
+//       var prettyResponse = JSON.stringify(response, null, "    ");
+//       responseContainer.html(prettyResponse);
+//     });
+//   }
