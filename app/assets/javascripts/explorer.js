f9d0c38e392c4e845a5a876456d03df8b6812acf
Powerglove <lang.joh.jl@gmail.com>
2016-09-29 15:06:25 +0200

    finish implementing endpoint links in JS

--- app/assets/javascripts/explorer.js
+++ app/assets/javascripts/explorer.js
@@ -8,44 +8,17 @@ $(function() {
       var prettyResponse = JSON.stringify(response, null, "    ");
       responseContainer.html(prettyResponse);
     });
-  }
+  };
 
   $("#request").keyup(makeRequest);
 
-  $('.endpoints').click(function(e) {
+  $('.endpoints a').click(function(e) {
     e.preventDefault();
-    makeRequest($(this)attr("href"))
-  })
+    $("#request").val($(this).attr("href"));
+    makeRequest();
+  });
 
 
 
 });
 
-
-
-
-// $(function() {
-//   $("#request").keyup(function(e) {
-//     var path = e.target.value;
-//     console.log(path);
-//   });
-// });
-//
-// $('.endpoints').click(
-//   function(e){
-//     e.preventDefault();
-//     var path = $(this).attr("href");
-//     $.getJSON(path, function(response) {
-//       var responseContainer = $("#explorer-response");
-//       var prettyResponse = JSON.stringify(response, null, "    ");
-//       responseContainer.html(prettyResponse);
-//       });
-//   }
-//
-//   function makeRequest(path) {
-//     $.getJSON(path, function(response) {
-//       var responseContainer = $("#explorer-response");
-//       var prettyResponse = JSON.stringify(response, null, "    ");
-//       responseContainer.html(prettyResponse);
-//     });
-//   }
