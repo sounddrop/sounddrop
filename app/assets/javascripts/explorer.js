3e55f1a3bab437d017438f86451cf29830bc92fe
Dayana Mick <dayanamick@gmail.com>
2016-10-11 11:15:20 +0200

    Delete commented out code, fix indentation, add missing semicolon

--- app/assets/javascripts/explorer.js
+++ app/assets/javascripts/explorer.js
@@ -1,16 +1,16 @@
 $(function() {
 
     var makeRequest = function(e) {
-    var path = $("#request").val();
-    console.log(path)
-    $.getJSON(path, function(response) {
-        var responseContainer = $("#explorer-response");
-        var prettyResponse = JSON.stringify(response, null, "    ");
-        responseContainer.html(prettyResponse);
-    });
+        var path = $("#request").val();
+        console.log(path);
+        $.getJSON(path, function(response) {
+            var responseContainer = $("#explorer-response");
+            var prettyResponse = JSON.stringify(response, null, "    ");
+            responseContainer.html(prettyResponse);
+        });
     };
 
-    // $("#request").keyup(makeRequest);
+
     $("#request").keyup( $.debounce( 800, makeRequest ));
 
 
